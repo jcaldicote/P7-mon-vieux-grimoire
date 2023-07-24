5fbf425a7840e2cab31529d66c7fd28b06d7f8b4
jcaldicote <caldicote.j@gmail.com>
2023-07-24 07:54:18 +0200

    améliration server.js

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+  console.log("requete reçu");
+  next();
+});
+app.use((req, res, next) => {
   res.json({ message: "Votre requête a bien été reçue !" });
 });
 
