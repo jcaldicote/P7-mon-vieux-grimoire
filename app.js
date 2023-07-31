da21b0143c7a5fa8daa500ffc8ea000183b75f2a
jcaldicote <caldicote.j@gmail.com>
2023-07-31 11:04:39 +0200

    clean code

--- app.js
+++ app.js
@@ -8,23 +8,6 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-// app.get("/", welcome);
-// app.post("/api/auth/signup", signUp);
-// app.post("/api/auth/log", logUser);
-
-// function welcome(req, res) {
-//   res.send(`*** Bienvenue sur le server Mon Vieux Grimoire ***`);
-// }
-
-// function signUp(req, res) {
-//   res.send("inscription OK");
-// }
-
-// function logUser(req, res) {
-//   const body = req.body;
-//   console.log("body content:", body);
-// }
-
 app.use("/api/auth", userRoutes);
 app.use("/api/books", bookRoutes);
 app.use("/images", express.static(path.join(__dirname, "images")));
